79ce1483650df9356dee7dd99b2d633f277a37da
Kleber Tiko <klebertiko@gmail.com>
2025-05-28 22:05:50 -0300

    fixed the base for my repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: '/tech-portfolio/', // Define a base URL para o GitHub Pages
 })
